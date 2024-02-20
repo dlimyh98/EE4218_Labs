1a9bc2828e554721274e8941daa1d7f7f28cbf08
dlimyh98 <dlimyh98@LAPTOP-51RINPU4>
2024-02-21 05:04:01 +0800

    V2.0 - matrixMult verified

--- Lab 2/Vitis/helloworld.c
+++ Lab 2/Vitis/helloworld.c
@@ -13,21 +13,22 @@
 #define NEW_LINE        0xA
 #define COMMA           0x2C
 #define CONCAT_BUFFER_SIZE 3
+#define RESULT_BUFFER_SIZE 4
 #define ONES        0
 #define TENS        1
 #define HUNDREDS    2
 
-#define A_NUM_ROWS 2
-#define A_NUM_COLS 2
-#define B_NUM_ROWS 2
+#define A_NUM_ROWS 64
+#define A_NUM_COLS 8
+#define B_NUM_ROWS 8
 #define B_NUM_COLS 1
 /*********************** FUNCTION DECLARATIONS **********************************/
 int init_UART(XUartPs* Uart_Ps);
 void override_uart_configs(XUartPs* Uart_Ps);
 
 void receive_from_realterm(u32 uart_base_addr, char* recv_a_matrix, char* recv_b_matrix);
-void do_processing(char* recv_a_matrix, char* recv_b_matrix, char* trans_res_matrix);
-void send_to_realterm(u32 uart_base_address, char* trans_res_matrix);
+void do_processing(char* recv_a_matrix, char* recv_b_matrix, int* trans_res_matrix);
+void send_to_realterm(u32 uart_base_address, int* trans_res_matrix);
 
 char concat_char_buffer(char* buffer_ptr, int tail_index);
 u8 find_place(u8 loop_iteration);
@@ -47,7 +48,7 @@ int main()
 
     char recv_a_matrix[A_NUM_ROWS*A_NUM_COLS] = {0};
     char recv_b_matrix[B_NUM_ROWS*B_NUM_COLS] = {0};
-    char trans_res_matrix[A_NUM_ROWS*B_NUM_COLS] = {0};
+    int trans_res_matrix[A_NUM_ROWS*B_NUM_COLS] = {0};
     
     xil_printf("Ready to accept files from Realterm\n");
     receive_from_realterm(UART_BASEADDR, recv_a_matrix, recv_b_matrix);
@@ -63,38 +64,54 @@ int main()
 
 
 /*********************************** FUNCTION DEFINITIONS *********************************************/
-void send_to_realterm(u32 uart_base_address, char* trans_res_matrix) {
+void send_to_realterm(u32 uart_base_address, int* trans_res_matrix) {
     // Additional +1 to accomodate null-termination of strings
     // Note string literals must be char, NOT unsigned char
-    char string[CONCAT_BUFFER_SIZE+1] = {-1};   
+    char string[RESULT_BUFFER_SIZE+1] = {-1};   
 
     for (int i = 0; i < A_NUM_ROWS*B_NUM_COLS; i++) {
         // If external libraries not allowed, could just do mathematical operations on elements of trans_res_matrix
+        // char a = '278'
+        // string[0] == '2', string[1] == '7', string[2] == '8'
+        // char b = '78'
+        // string[0] == '7', string[1] == '8' 
+        // char c = '8'
+        // string[0] == '8'
         sprintf(string, "%d", trans_res_matrix[i]);
 
-		XUartPs_WriteReg(uart_base_address, XUARTPS_FIFO_OFFSET, string[0]);
-		XUartPs_WriteReg(uart_base_address, XUARTPS_FIFO_OFFSET, string[1]);
+        // Common to all 1/2/3/4 digit numbers
+        while (XUartPs_IsTransmitFull(uart_base_address));
+        XUartPs_WriteReg(uart_base_address, XUARTPS_FIFO_OFFSET, string[0]);
 
+        // Common to all 2/3/4 digit numbers
+        if (trans_res_matrix[i] >= 10) {
+            while (XUartPs_IsTransmitFull(uart_base_address));
+            XUartPs_WriteReg(uart_base_address, XUARTPS_FIFO_OFFSET, string[1]);
+        }
+
+        // Common to all 3/4 digit numbers
         if (trans_res_matrix[i] >= 100) {
+            while (XUartPs_IsTransmitFull(uart_base_address));
             XUartPs_WriteReg(uart_base_address, XUARTPS_FIFO_OFFSET, string[2]);
         }
 
-        // We don't need ',' after printing the last element
-        if (i != (A_NUM_ROWS*B_NUM_COLS)-1) {
-            XUartPs_WriteReg(uart_base_address, XUARTPS_FIFO_OFFSET, COMMA);
+        // Solely for 4 digit numbers
+        if (trans_res_matrix[i] >= 1000) {
+            while (XUartPs_IsTransmitFull(uart_base_address));
+            XUartPs_WriteReg(uart_base_address, XUARTPS_FIFO_OFFSET, string[3]);
         }
-    }
 
-    // Finish off with a newline
-    XUartPs_WriteReg(uart_base_address, XUARTPS_FIFO_OFFSET, NEW_LINE);
+        // Newline for next element
+        while (XUartPs_IsTransmitFull(uart_base_address));
+        XUartPs_WriteReg(uart_base_address, XUARTPS_FIFO_OFFSET, NEW_LINE);
+    }
 }
 
 
-void do_processing(char* recv_a_matrix, char* recv_b_matrix, char* trans_res_matrix) {
+void do_processing(char* recv_a_matrix, char* recv_b_matrix, int* trans_res_matrix) {
     for (int i = 0; i < A_NUM_ROWS; i++) {
         // Note we use int to prevent overflow
-        // Elements of the input matrices being between 0 and 127 will guarantee that 
-        // the result will not exceed the representation range possible with 8-bits. n*127*127/256, where n = 4 is less than 255.
+        // Elements of the input matrices are between 0 and 255, ( (255*255)*(NUM_A_COLS) ) >> 8 = 2032.03125
         int sum = 0;
 
         for (int j = 0; j < A_NUM_COLS; j++) {
@@ -122,7 +139,7 @@ void do_processing(char* recv_a_matrix, char* recv_b_matrix, char* trans_res_mat
 void receive_from_realterm(u32 uart_base_addr, char* recv_a_matrix, char* recv_b_matrix) {
     int valid_recv_count = 0;    // Number of elements in our usual Matrix
 
-    char buffer[CONCAT_BUFFER_SIZE] = {0};  // Maximal integer value is '255', formed by 3 chars
+    char buffer[CONCAT_BUFFER_SIZE] = {0};  // Maximal incoming matrix value is '255', formed by 3 chars
     int num_insertions = 0;
 
     while(1) {
@@ -137,16 +154,12 @@ void receive_from_realterm(u32 uart_base_addr, char* recv_a_matrix, char* recv_b
         // Read from Transmit and Receive FIFO[7:0]
         u8 recv_char = XUartPs_ReadReg(uart_base_addr, XUARTPS_FIFO_OFFSET);
 
-        //xil_printf("Received %c\n", recv_char);
-
         // Parse the data accordingly
         if (recv_char == NEW_LINE || recv_char == COMMA) {
             // Newline, means we are going to 'next row' of matrix
             // Comma, means we are transitioning to next matrix 'element'
             u8 concat_char = concat_char_buffer(buffer, num_insertions-1);
             
-            //xil_printf("concatChar %c\n", concat_char);
-
             if (valid_recv_count < A_NUM_ROWS*A_NUM_COLS) {
                 *recv_a_matrix = concat_char;
                 recv_a_matrix++;
