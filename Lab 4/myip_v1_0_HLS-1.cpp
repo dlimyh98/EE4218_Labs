ccd5286c3addac6ecae7d8ed1ec5c23a6457a2c6
dlimyh98 <dlimyh98@LAPTOP-51RINPU4>
2024-03-20 13:44:06 +0800

    V1.0 - AXI FIFO baseline

--- Lab 4/myip_v1_0_HLS-1.cpp
+++ Lab 4/myip_v1_0_HLS-1.cpp
@@ -1,73 +1,81 @@
-/*
-----------------------------------------------------------------------------------
---	(c) Rajesh C Panicker, NUS,
---  Description : AXI Stream Coprocessor (HLS), implementing the sum of 4 numbers
---	License terms :
---	You are free to use this code as long as you
---		(i) DO NOT post a modified version of this on any public repository;
---		(ii) use it only for educational purposes;
---		(iii) accept the responsibility to ensure that your implementation does not violate any intellectual property of any entity.
---		(iv) accept that the program is provided "as is" without warranty of any kind or assurance regarding its suitability for any particular purpose;
---		(v) send an email to rajesh.panicker@ieee.org briefly mentioning its use (except when used for the course EE4218 at the National University of Singapore);
---		(vi) retain this notice in this file or any files derived from this.
-----------------------------------------------------------------------------------
-*/
-
 #include "hls_stream.h"
 #include "ap_int.h"
 #include "ap_axi_sdata.h"
 
-#define NUMBER_OF_INPUT_WORDS 4  // length of an input vector
-#define NUMBER_OF_OUTPUT_WORDS 4  // length of an input vector
+#define NUMBER_OF_INPUT_WORDS 520
+#define NUMBER_OF_OUTPUT_WORDS 64
+#define A_NUM_ROWS 64
+#define A_NUM_COLS 8
+#define B_NUM_ROWS 8
+#define B_NUM_COLS 1
 
-// Creating a custom structure which includes the data word and TLAST signal.
 // ACLK, ARESETN, TREADY, TDATA, TVALID are essential signals for AXIS.
 // TLAST is a sideband signal which is optional in AXIS.
 // However, it is necessary for us since we connecting M_AXIS to AXI Stream FIFO / AXI DMA.
-// So, we create a struct with data (TDATA) and last (TLAST). The rest of the essential AXIS signals are automatically dealt with by the HLS tool.
 
-
-/* // doesn't work with HLS version 2022.2
-struct AXIS_wLAST{
-	int data;
-	//ap_uint<32> data;
-	bool last;
-};
-*/
-
+// Declare an AXI-4 Stream interface (without side-channels)
 typedef ap_axis<32,0,0,0> AXIS_wLAST;
 
+// https://docs.amd.com/r/en-US/ug1399-vitis-hls/Interfaces-for-Vitis-Kernel-Flow
+// https://docs.amd.com/r/en-US/ug1399-vitis-hls/AXI4-Stream-Interfaces
+// Since we are using AXI-4 Stream interface protocol, the argument is hls::stream (Paradigm is Stream)
 void myip_v1_0_HLS(hls::stream<AXIS_wLAST>& S_AXIS, hls::stream<AXIS_wLAST>& M_AXIS){
-#pragma HLS INTERFACE ap_ctrl_none port=return
-#pragma HLS INTERFACE axis port=S_AXIS
-#pragma HLS INTERFACE axis port=M_AXIS
 
-	int word_cnt;
-	ap_uint<8> sum = 0; // using arbitrary precision
-	//int sum = 0;		 // using 32 bit precision
-	AXIS_wLAST read_input, write_output;
+	// https://docs.amd.com/r/en-US/ug1399-vitis-hls/Introduction-to-Interface-Synthesis
+	#pragma HLS INTERFACE ap_ctrl_none port=return  // https://docs.amd.com/r/2022.1-English/ug1399-vitis-hls/Using-ap_ctrl_none-Inside-the-Dataflow
+													// port=return is what the HLS calls the control interface of synthesized IP block
+	#pragma HLS INTERFACE axis port=S_AXIS			// implement port as AXI-4 Stream interface
+	#pragma HLS INTERFACE axis port=M_AXIS			// implement port as AXI-4 Stream interface
 
-		myip_v1_0_HLS_for1:for(word_cnt = 0; word_cnt < NUMBER_OF_INPUT_WORDS; word_cnt++){
-			read_input = S_AXIS.read();
-			// read_input is the element (data + other signals) received by our ip through S_AXIS in one clock cycle (which contains one word).
-			// read() extracts it from the stream. Overloaded operator >> can also be used.
-			sum += read_input.data; //extracting that word
-			// We are not making using of S_AXIS_TLAST in this example.
-			// S_AXIS_TLAST is required only when we are receiving an unknown number of words.
+	// Input matrices maximally 255
+	// Output matrix maximally [(255*255)*(A_NUM_COLS)] >> 8
+	ap_uint<8> recv_a_matrix[A_NUM_ROWS*A_NUM_COLS] = {0};
+	ap_uint<8> recv_b_matrix[B_NUM_ROWS*B_NUM_COLS] = {0};
+	ap_uint<12> trans_res_matrix[A_NUM_ROWS*B_NUM_COLS] = {0};
+
+	ap_uint<32> sum = 0;				// (255*255)*(NUM_A_COLS) = 4161600
+
+	AXIS_wLAST read_input;
+	AXIS_wLAST write_output;
+
+	// We are not making using of S_TLAST (from Master) when Coprocessor (Slave) receives Data
+	// S_AXIS_TLAST is required only when we are receiving an unknown number of words.
+	myip_v1_0_HLS_receive:for(int word_cnt = 0; word_cnt < NUMBER_OF_INPUT_WORDS; word_cnt++){
+		// read_input is the element (data + other signals) received by our IP through S_AXIS in one clock cycle (which contains one word).
+		// read() extracts it from the stream. Overloaded operator >> can also be used.
+		read_input = S_AXIS.read();
+
+		if (word_cnt < A_NUM_ROWS*A_NUM_COLS) {
+			recv_a_matrix[word_cnt] = read_input.data;	  // Extract the word
+		}
+		else if (A_NUM_ROWS*A_NUM_COLS <= word_cnt
+				&& word_cnt < A_NUM_ROWS*A_NUM_COLS + B_NUM_ROWS*B_NUM_COLS) {
+			recv_b_matrix[word_cnt-(A_NUM_ROWS*A_NUM_COLS)] = read_input.data;
+		}
+	}
+
+
+	myip_v1_0_HLS_processing_outer:for(int i = 0; i < A_NUM_ROWS; i++) {
+		sum = 0;
+
+		myip_v1_0_HLS_processing_inner:for(int j = 0; j < A_NUM_COLS; j++) {
+			sum += recv_a_matrix[(i*A_NUM_COLS)+j] * recv_b_matrix[j];
 		}
 
-		myip_v1_0_HLS_for2:for(word_cnt = 0; word_cnt < NUMBER_OF_OUTPUT_WORDS; word_cnt++){
-			//write_output.data = sum.to_int() + word_cnt;	// using arbitrary precision internally but int for interfacing
-			write_output.data = sum + word_cnt;	// using 32 bit precision or arbitrary precision all the way
-			// write_output is the element sent by our ip through M_AXIS in one clock cycle.
-			write_output.last = 0;
-			if(word_cnt==NUMBER_OF_OUTPUT_WORDS-1)
-			{
-				write_output.last = 1;
-				// M_AXIS_TLAST is required to be asserted for the last word.
-				// Else, the AXI Stream FIFO / AXI DMA will not know if all the words have been received from the co-processor.
-			}
-			M_AXIS.write(write_output);
-			// write() inserts it into the stream. Overloaded operator << can also be used.
-		}
+		trans_res_matrix[i] = (sum >> 8);
+	}
+
+
+	myip_v1_0_HLS_transmit:for(int word_cnt = 0; word_cnt < NUMBER_OF_OUTPUT_WORDS; word_cnt++){
+		// M_TLAST (AXI4-Stream signal) required for connection to AXI DMA
+		// M_TLAST is required to be asserted for the last word.
+		// Else, the AXI Stream FIFO / AXI DMA will not know if all the words have been received from the co-processor.
+		write_output.last = (word_cnt==NUMBER_OF_OUTPUT_WORDS-1) ? 1 : 0;
+
+		// write_output is the element sent by our IP through M_AXIS in one clock cycle.
+		write_output.data = trans_res_matrix[word_cnt];
+
+		// write() inserts it into the stream. Overloaded operator << can also be used.
+		M_AXIS.write(write_output);
+	}
 }
